035b401ee9e3f8399dd6af7ce55169b8511fa767
Fajri <spag.959@gmail.com>
2025-12-20 07:31:22 +0700

    refactor: Simplify venue query and remove unused delete confirmation logic

--- app/Livewire/Dashboard/Venues/Venues.php
+++ app/Livewire/Dashboard/Venues/Venues.php
@@ -5,18 +5,17 @@ namespace App\Livewire\Dashboard\Venues;
 use App\Models\Venue;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Layout('layouts.dashboard', ['title' => 'Tempat'])]
 class Venues extends Component
 {
+    use WithPagination;
     public string $search = '';
 
-    public ?int $venueId = null;
-
-    public function confirmDelete($venueId)
+    public function updatedSearch()
     {
-        $this->venueId = $venueId;
-        $this->dispatch('open-delete-modal');
+        $this->resetPage();
     }
 
     public function toggleStatus($venueId)
@@ -32,13 +31,20 @@ class Venues extends Component
 
     public function render()
     {
-        $venues = Venue::query()
+        $venues = Venue::select(
+            'id',
+            'name',
+            'address',
+            'capacity',
+            'price_per_hour',
+            'status'
+        )
             ->when($this->search, function ($query) {
                 $query->where('name', 'like', "%{$this->search}%")
                     ->orWhere('city', 'like', "%{$this->search}%");
             })
             ->latest()
-            ->get();
+            ->paginate(10);
 
         return view('livewire.dashboard.venues.venues', compact('venues'));
     }
