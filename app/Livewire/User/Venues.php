b3614e810dac3c3200fb88fb1ce45754511dcc5e
Fajri <spag.959@gmail.com>
2025-12-20 08:32:44 +0700

    feat: Implement search functionality in Venues component with pagination

--- app/Livewire/User/Venues.php
+++ app/Livewire/User/Venues.php
@@ -2,14 +2,31 @@
 
 namespace App\Livewire\User;
 
+use App\Models\Venue;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Layout('layouts.app', ['title' => 'Cari Tempat'])]
 class Venues extends Component
 {
+    use WithPagination;
+
+    public string $search = '';
+
+    protected $queryString = ['search'];
+
     public function render()
     {
-        return view('livewire.user.venues');
+        $venues = Venue::query()
+            ->where('status', 'available')
+            ->when($this->search, function ($query) {
+                $query->where('name', 'like', "%{$this->search}%")
+                    ->orWhere('city', 'like', "%{$this->search}%");
+            })
+            ->latest()
+            ->paginate(9);
+
+        return view('livewire.user.venues', compact('venues'));
     }
 }
