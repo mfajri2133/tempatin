89691e1a9a3963b4646ca426c4e94539faae0f1c
Fajri <spag.959@gmail.com>
2025-12-21 10:59:34 +0700

    fix: update venue detail view for improved image handling and status display

--- resources/views/livewire/dashboard/venues/venue-detail.blade.php
+++ resources/views/livewire/dashboard/venues/venue-detail.blade.php
@@ -2,26 +2,27 @@
     <!-- Header -->
     <div class="flex flex-col gap-4 sm:flex-row items-center justify-between p-4 bg-white border-b border-gray-200">
         <h2 class="text-lg font-semibold text-gray-700">Informasi Tempat</h2>
-        <a href="{{ route('dashboard.venues.index') }}" 
-           wire:navigate
-           class="bg-blue-100 text-blue-600 hover:bg-blue-200 focus:ring-blue-300 inline-flex items-center gap-2 px-4 py-2 rounded text-sm transition whitespace-nowrap text-center justify-center w-full sm:w-auto">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <a href="{{ route('dashboard.venues.index') }}" wire:navigate
+            class="bg-blue-100 text-blue-600 hover:bg-blue-200 focus:ring-blue-300 inline-flex items-center gap-2 px-4 py-2 rounded text-sm transition whitespace-nowrap text-center justify-center w-full sm:w-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
             </svg>
             Kembali
         </a>
     </div>
-   
-   <!-- Image -->
+
+    <!-- Image -->
     <div class="w-full h-100 sm:h-80 md:h-96 overflow-hidden">
-        @if($venue->image) 
-            <img src="{{ asset('storage/' . $venue->image) }}" 
-                 alt="{{ $venue->name }}" 
-                 class="w-full h-full object-cover">
+        @if ($venue->venue_img)
+            <img src="{{ asset('storage/' . $venue->venue_img) }}" alt="{{ $venue->name }}"
+                class="w-full h-full object-cover">
         @else
             <div class="w-full h-full bg-gray-200 flex items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="size-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6.75a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6.75v10.5a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                <svg xmlns="http://www.w3.org/2000/svg" class="size-12 text-gray-400" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                        d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6.75a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6.75v10.5a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                 </svg>
             </div>
         @endif
@@ -32,8 +33,9 @@
         <div class="flex-1">
             <p class="text-m font-semibold text-gray-600">Nama Tempat</p>
             <h2 class="text-2xl font-extrabold text-gray-800 mb-2">{{ $venue->name }}</h2>
-            <span class="inline-flex items-center px-3 py-1 text-xs rounded-full {{ $venue->status === 'available' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700' }}">
-                {{($venue->status) }}
+            <span
+                class="inline-flex items-center px-3 py-1 text-xs rounded-full {{ $venue->status === 'available' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700' }}">
+                {{ $venue->status }}
             </span>
         </div>
 
