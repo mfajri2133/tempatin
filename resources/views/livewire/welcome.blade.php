fc218fe13ef47a65860dd0b0d8d97f8b429c8791
mildakhaerunnisa <milda.233040125@mail.unpas.ac.id>
2025-12-22 23:25:29 +0700

    feat: landing section one

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -19,4 +19,48 @@
             </p>
         </div>
     </section>
+
+    <section class="py-10 md:py-12 bg-white overflow-hidden">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+                <div class="order-2 lg:order-1">
+                    <h1 class="text-4xl md:text-6xl font-black text-gray-100 tracking-tighter leading-none">
+                        <span class="text-indigo-600">RUANG TERBAIK</span>
+                    </h1>
+                    <p class="mt-8 text-gray-600 text-lg max-w-md leading-relaxed font-medium">
+                        Temukan Venue berkualitas di Jawa Barat untuk setiap kebutuhan seperti Corporate Event, Celebration, hingga Sports Activity.
+                    </p>
+                    
+                    <div class="flex flex-wrap gap-x-6 gap-y-4 border-t border-gray-800 pt-10 mt-10">
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Aula</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Co-Working Space</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Convention Hall</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Event Space</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Gedung Serbaguna</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Lapangan</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Outdoor Venue</span>
+                        <span class="text-gray-500 text-xs uppercase  font-bold hover:text-indigo-500">Ruang Meeting</span>
+                    </div>
+                </div>
+
+                <div class="order-1 lg:order-2 flex gap-4 h-100 md:h-100">
+                    <div class="w-2/3 rounded-2xl overflow-hidden relative group shadow-2xl">
+                        <img src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?q=80&w=1000&auto=format&fit=crop" 
+                            class="w-full h-full object-cover">
+                    </div>
+
+                    <div class="w-1/3 flex flex-col gap-4">
+                        <div class="h-1/2 rounded-2xl overflow-hidden relative group shadow-xl">
+                            <img src="https://images.unsplash.com/photo-1620735692151-26a7e0748429?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                                class="w-full h-full object-cover">
+                        </div>
+                        <div class="h-1/2 rounded-2xl overflow-hidden relative group shadow-xl">
+                            <img src="https://images.unsplash.com/photo-1715333157357-a8e4acdd7992?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                class="w-full h-full object-cover">    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
