6fe5de15a2d29c39598d040e2f0a0e74cd18baf3
Fajri <spag.959@gmail.com>
2025-12-26 11:16:28 +0700

    feat: update payment total display style for better visibility

--- resources/views/livewire/user/order-payment.blade.php
+++ resources/views/livewire/user/order-payment.blade.php
@@ -44,7 +44,7 @@
                 <hr>
 
                 <div class="flex justify-between text-base font-bold">
-                    <span>Total Pembayaran</span>
+                    <span class="text-black">Total Pembayaran</span>
                     <span class="text-indigo-600">
                         Rp {{ number_format($order->total_amount, 0, ',', '.') }}
                     </span>
