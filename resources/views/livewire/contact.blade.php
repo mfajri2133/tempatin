873b79e9bc4129542c95189a299481159942417d
mildakhaerunnisa <milda.233040125@mail.unpas.ac.id>
2025-12-27 15:20:27 +0700

    feat: add contact page

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,3 +1,61 @@
-<div>
-    {{-- Be like water. --}}
+<div class="bg-[#0f0f0f] py-12">
+    <div class="max-w-6xl mx-auto px-4">
+        <div class="mb-8 border-indigo-600">
+            <h1 class="text-4xl font-black text-white text-center">
+                Hubungi <span class="text-indigo-600">Kami</span>
+            </h1>
+            <p class="text-gray-400 text-center mt-2 text-m ">
+                Kami hadir untuk membantu. Silakan kirimkan pesan Anda melalui formulir atau hubungi kami melalui kontak dibawah ini.
+            </p>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div class="md:col-span-1 space-y-5 mt-2">
+                <h3 class="text-xl font-black text-white">
+                    Kontak Kami
+                </h3>
+                <p class="text-gray-400 text-sm mx-auto">
+                    Terhubung langsung dengan tim dan media sosial resmi kami.
+                </p>
+
+                <div class="space-y-4">
+                    <div class="flex items-center gap-3">
+                        <div class="text-indigo-500">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
+                        </div>
+                        <span class="text-gray-300 text-sm">tempatin.web@gmail.com</span>
+                    </div>
+
+                    <div class="flex items-center gap-3">
+                        <div class="text-indigo-500">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                            </svg>
+                        </div>
+                        <a href="https://www.instagram.com/tempatin.my.id?igsh=ZGFjeTRva2lpbXph" target="_blank" class="text-gray-300 text-sm hover:text-white transition-colors">
+                            @tempatin.my.id
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="md:col-span-2">
+                <form action="#" method="POST" class="grid grid-cols-1 gap-4">
+                @csrf
+                    <input type="text" placeholder="Nama Lengkap" 
+                        class="bg-gray-900/50 text-white text-sm rounded-xl p-4 outline-none focus:ring-1 focus:ring-indigo-600 transition-all">
+                     <input type="email" placeholder="Alamat Email" 
+                        class="bg-gray-900/50 text-white text-sm rounded-xl p-4 outline-none focus:ring-1 focus:ring-indigo-600 transition-all">
+                    <textarea rows="4" placeholder="Apa yang bisa kami bantu?" 
+                        class="w-full bg-gray-900/50 text-white text-sm rounded-xl p-4 outline-none focus:ring-1 focus:ring-indigo-600 transition-all resize-none"></textarea>
+                    <button type="submit" 
+                        class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase text-xs tracking-widest py-4 rounded-xl transition-all shadow-lg shadow-indigo-600/10 active:scale-[0.98]">
+                        Kirim Sekarang
+                    </button>
+                </form>
+            </div> 
+        </div>
+    </div>
 </div>
