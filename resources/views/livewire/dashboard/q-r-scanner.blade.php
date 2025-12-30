3cd0d9be2c6b354aeab09bf963557d22d257f972
Hanief <aryaraihanhanif@gmail.com>
2025-12-30 17:23:10 +0700

    feat: add scanner ui (no function)

--- resources/views/livewire/dashboard/q-r-scanner.blade.php
+++ resources/views/livewire/dashboard/q-r-scanner.blade.php
@@ -1,3 +1,14 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="min-h-[calc(100vh-12rem)] flex items-center justify-center">
+    <div class="w-full max-w-xl aspect-square bg-white rounded-lg border border-tp-black overflow-hidden">
+        <div class="relative w-full h-full bg-gray-50" wire:ignore>
+            <video id="qrVideo" class="absolute inset-0 w-full h-full object-cover" autoplay muted playsinline></video>
+            <div class="absolute inset-0 flex items-center justify-center bg-gray-50">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-gray-300" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6.827 6.175A2.31 2.31 0 0 1 8.417 5.5h7.166c.579 0 1.14.217 1.59.675l1.302 1.325c.11.112.28.174.456.174H20.5a1.5 1.5 0 0 1 1.5 1.5v9.326a1.5 1.5 0 0 1-1.5 1.5H3.5A1.5 1.5 0 0 1 2 18.5V9.174a1.5 1.5 0 0 1 1.5-1.5h1.57c.176 0 .346-.062.456-.174l1.301-1.325ZM15.75 13.5a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                </svg>
+            </div>
+        </div>
+    </div>
 </div>
